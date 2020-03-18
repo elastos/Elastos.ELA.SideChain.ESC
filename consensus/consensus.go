3a2a4ac1ea0857f71ca3dd2c67c11498d99b946d
ttblack <zhangxiaobin@elastos.org>
2020-03-18 17:32:48 +0800

    lightchain add evil record

--- consensus/consensus.go
+++ consensus/consensus.go
@@ -47,6 +47,9 @@ type ChainReader interface {
 
 	// GetBlock retrieves a block from the database by hash and number.
 	GetBlock(hash common.Hash, number uint64) *types.Block
+
+	// IsDangerChain return whether to many evil singers.
+	IsDangerChain() bool
 }
 
 // Engine is an algorithm agnostic consensus engine.
