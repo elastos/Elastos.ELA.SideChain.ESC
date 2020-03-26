3d44ee6f1f3f0dc6d6daa214a48e1c9706069f35
ttblack <zhangxiaobin@elastos.org>
2020-03-26 21:58:43 +0800

    fix evilSigner_test.go

--- core/evilSigner_test.go
+++ core/evilSigner_test.go
@@ -93,11 +93,11 @@ func TestRemoveOldEvilSigners(t *testing.T) {
 				signersNum, evilEventsNum, len(*evilMaps))
 		}
 
-		evilMaps2 := evilMaps.GetEvilSignerEvents()
-		if len(evilMaps2) != len(*evilMaps) {
+		events := evilMaps.GetEvilSignerEvents()
+		if len(events) != evilEventsNum - 1 {
 			t.Errorf("GetEvilSignerEvents error")
 		}
-		for _, v := range evilMaps2 {
+		for _, v := range events {
 			if (*evilMaps)[*v.Singer] == nil {
 				t.Errorf("GetEvilSignerEvents error")
 			}
