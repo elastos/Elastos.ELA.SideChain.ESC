981b29d57dea2409c537183dccae5c159780a8d3
ttblack <zhangxiaobin@elastos.org>
2020-04-10 21:46:30 +0800

    add proposal and test

---
+++ consensus/pbft/proposal_test.go
@@ -0,0 +1,73 @@
+package pbft
+
+import (
+	"bytes"
+	"crypto/rand"
+	"os"
+
+	"github.com/elastos/Elastos.ELA.SideChain.ETH/accounts/keystore"
+	"github.com/elastos/Elastos.ELA.SideChain.ETH/common"
+	"github.com/elastos/Elastos.ELA.SideChain.ETH/rlp"
+
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func randomDposProposal() *Proposal {
+	var sponsor = make([]byte, len(common.Address{}))
+	rand.Read(sponsor)
+	blockHash := make([]byte, len(common.Hash{}))
+	rand.Read(blockHash)
+
+	sign := make([]byte, 65)
+	rand.Read(sign)
+	return &Proposal{
+		common.BytesToAddress(sponsor),
+		common.BytesToHash(blockHash),
+		0,
+		sign,
+		nil,
+	}
+}
+
+func TestProposalEncodeRLP(t *testing.T) {
+	proposal1 := randomDposProposal()
+	buf := new(bytes.Buffer)
+	err := proposal1.RlpEncode(buf)
+	if err != nil {
+		t.Error("RlpEncode err:", err.Error())
+	}
+	proposal2 := &Proposal{}
+	reader := bytes.NewReader(buf.Bytes())
+	stream := rlp.NewStream(reader, uint64(reader.Len()))
+	err = proposal2.RlpDecode(stream)
+	if err != nil {
+		t.Error("RlpDecode err:", err.Error())
+	}
+	assert.True(t, bytes.Equal(proposal1.Sponsor.Bytes(), proposal2.Sponsor.Bytes()))
+	assert.True(t, proposal1.BlockHash == proposal2.BlockHash)
+	assert.True(t, 	proposal1.ViewOffset == proposal2.ViewOffset)
+	assert.True(t, bytes.Equal(proposal1.Hash().Bytes(), proposal2.Hash().Bytes()))
+}
+
+func TestStartAndCheckProposal(t *testing.T) {
+	keystore := keystore.NewKeyStore("keystore",keystore.StandardScryptN, keystore.StandardScryptP)
+	defer os.RemoveAll("keystore")
+	account, err := keystore.NewAccount("123")
+	if err != nil {
+		t.Error(err)
+	}
+	wallet := keystore.Wallets()[0]
+	accountWallet := NewAccount(wallet, &account)
+	err = keystore.Unlock(account, "123")
+	assert.True(t, err == nil)
+
+	data := make([]byte, 32)
+	rand.Read(data)
+	blockHash := common.BytesToHash(data)
+	proposal, err := StartProposal(accountWallet, &blockHash)
+	assert.True(t, err == nil)
+
+	err = CheckProposal(proposal)
+	assert.True(t, err == nil)
+}
