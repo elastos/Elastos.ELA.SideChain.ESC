21afe78e98647a5ebd3317274cca68bb77572a0b
ttblack <zhangxiaobin@elastos.org>
2020-10-15 17:25:17 +0800

    modify networkid

--- eth/config.go
+++ eth/config.go
@@ -43,7 +43,7 @@ var DefaultConfig = Config{
 		DatasetsInMem:  1,
 		DatasetsOnDisk: 2,
 	},
-	NetworkId:          1,
+	NetworkId:          20,
 	LightPeers:         100,
 	UltraLightFraction: 75,
 	DatabaseCache:      512,
