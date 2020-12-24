277a3ad70c304eec32892cf2eaacb3410a6be783
ttblack <zhangxiaobin@elastos.org>
2020-12-24 10:44:01 +0800

    delete announce log

--- eth/peer.go
+++ eth/peer.go
@@ -127,7 +127,6 @@ func (p *peer) broadcast() {
 				return
 			}
 			p.Log().Trace("Propagated block", "number", prop.block.Number(), "hash", prop.block.Hash(), "td", prop.td)
-			p.Log().Info("Propagated block", "number", prop.block.Number(), "hash", prop.block.Hash(), "td", prop.td)
 		case block := <-p.queuedAnns:
 			if err := p.SendNewBlockHashes([]common.Hash{block.Hash()}, []uint64{block.NumberU64()}); err != nil {
 				return
