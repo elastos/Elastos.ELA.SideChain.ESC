df3603b1a3bdd397b419fc9bc49a503a9b96e7ac
xuxinlai2002 <xxl@local.com>
2021-03-07 20:51:45 +0800

    add rpc and web3 console call for did_resolveCredential

--- internal/web3ext/web3ext.go
+++ internal/web3ext/web3ext.go
@@ -854,13 +854,22 @@ property: 'did',
 
 methods: [
 
-new web3._extend.Method({
-
-	name: 'resolveDID',
-	call: 'did_resolveDID',
-	params: 2
-
-}),
+	new web3._extend.Method({
+	
+		name: 'resolveDID',
+		call: 'did_resolveDID',
+		params: 2
+	
+	}),
+	
+	
+	new web3._extend.Method({
+	
+		name: 'resolveCredential',
+		call: 'did_resolveCredential',
+		params: 2
+	
+	}),
 
 ]
 
