7fc5ca8705a8744c625c086e5926d169ff6a3535
ttblack <zhangxiaobin@elastos.org>
2021-06-15 15:01:01 +0800

    add did proxy contract

--- core/vm/did/did.sol
+++ core/vm/did/did.sol
@@ -1,8 +1,6 @@
 pragma solidity >=0.7.0 <0.8.0;
 pragma experimental ABIEncoderV2;
-
 contract DID {
-
     constructor() {
 
     }
@@ -16,7 +14,7 @@ contract DID {
 
         // string memory ttype = string(toBytesEth(txType));
         // string memory input = strConcat(ttype, didDocument);
-        string memory input = didDocument;
+        string memory input = data;
         inputSize = bytes(input).length + offSet;
 
         assembly {
@@ -29,6 +27,36 @@ contract DID {
         }
     }
 
+// '{"did":"did:elastos:iTWqanUovh3zHfnExGaan4SJAXG3DCZC6j","all":true, "branch": ["transaction", 0, "txid"]}'
+    function resolveDID(string memory paramsJson) public returns (string memory) {
+        uint method = 23;
+        uint offSet = 32;
+        uint outputSize = 3200;
+        bytes32[100] memory result;
+        uint256 inputSize = 0;
+        uint256  leftGas =  gasleft();
+
+
+        inputSize = bytes(paramsJson).length + offSet;
+
+        assembly {
+            if iszero(staticcall(leftGas, method, paramsJson, inputSize, result, outputSize)) {
+                //  revert(0,0)
+            }
+        }
+        string memory content = "";
+        for (uint8 i = 0; i < 38; i++) {
+            string memory b = Bytes32ToString(result[i]);
+            if(result[i] == "") {
+                break;
+            }
+            content = strConcat(content, b );
+        }
+
+       return content;
+    }
+
+
     function toBytesEth(uint256 x) private returns (bytes memory b) {
         b = new bytes(32);
         for (uint i = 0; i < 32; i++) {
@@ -36,7 +64,7 @@ contract DID {
         }
     }
 
-     function strConcat(string memory a, string memory b) private returns (string memory) {
+    function strConcat(string memory a, string memory b) private returns (string memory) {
         bytes memory ba = bytes(a);
         bytes memory bb = bytes(b);
         string memory ret = new string(ba.length + bb.length);
@@ -45,5 +73,23 @@ contract DID {
         for (uint i = 0; i < ba.length; i++)bret[k++] = ba[i];
         for (uint i = 0; i < bb.length; i++) bret[k++] = bb[i];
         return ret;
-   }
+    }
+
+  function Bytes32ToString(bytes32 b32name) private returns(string memory){
+        bytes memory bytesString = new bytes(32);
+        uint charCount = 0;
+        for(uint32 i = 0; i < 32; i++){
+            byte char = byte(bytes32(uint(b32name) * 2 ** (8 * i)));
+            if(char != 0){
+                bytesString[charCount] = char;
+                charCount++;
+            }
+        }
+
+        bytes memory bytesStringTrimmed = new bytes(charCount);
+        for(uint i = 0;i< charCount;i++){
+            bytesStringTrimmed[i] = bytesString[i];
+        }
+        return string(bytesStringTrimmed);
+    }
 }
