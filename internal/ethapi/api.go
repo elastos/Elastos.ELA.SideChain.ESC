ttblack/Elastos.ELA.SideChain.ETH

add withdraw refund tx

internal/ethapi/api.go
@@ -49,6 +49,7 @@ import (
 	"github.com/elastos/Elastos.ELA.SideChain.ETH/rpc"
 	"github.com/elastos/Elastos.ELA.SideChain.ETH/smallcrosstx"
 	"github.com/elastos/Elastos.ELA.SideChain.ETH/spv"
+	"github.com/elastos/Elastos.ELA.SideChain.ETH/withdrawfailedtx"
 
 	"github.com/elastos/Elastos.ELA/events"
 )
@@ -779,6 +780,25 @@ func (s *PublicBlockChainAPI) GetFailedRechargeTxs(ctx context.Context, height u
 func (s *PublicBlockChainAPI) GetFailedRechargeTxByHash(ctx context.Context, hash string) (string, error) {
 	txid := spv.GetFailedRechargeTxByHash(hash)
 	return txid, nil
+}
+
+func (s *PublicBlockChainAPI) SendInvalidWithdrawTransaction(ctx context.Context, signature string, hash string) error {
+	txid := common.HexToHash(hash)
+	tx, _, _, _, err := s.b.GetTransaction(ctx, txid)
+	if tx == nil || err != nil {
+		msg := fmt.Sprintf("not found withdraw tx, txid:%s", hash)
+		log.Error(msg)
+		return errors.New(msg)
+	}
+	if tx.To().String() != s.b.ChainConfig().BlackContractAddr {
+		msg := fmt.Sprintf("is not withdraw tx, txid:%s", hash)
+		return errors.New(msg)
+	}
+	err = withdrawfailedtx.ReceivedFailedWithdrawTx(hash, signature)
+	if err != nil {
+		log.Error("ReceivedFailedWithdrawTx", "error", err)
+	}
+	return err
 }
 
 // CallArgs represents the arguments for a call.

oracle/receivedInvaliedwithrawtx.js
@@ -1,0 +1,40 @@
+"use strict";
+
+const common = require("./common");
+const child_process = require("child_process");
+
+module.exports = async function (json_data, res) {
+    try {
+        let signature = json_data["params"]["signature"];
+        let failedTx = json_data["params"]["txHash"];
+        let host = common.web3._provider.host;
+
+        let txprocessed = await common.web3.eth.getStorageAt(common.blackAdr, failedTx, common.latest)
+        if (txprocessed != common.zeroHash64) {
+            console.log("Failed Withdraw Trasaction Hash already processed: " + txprocessed);
+            console.log("============================================================");
+            res.json({"error": null, "id": null, "jsonrpc": "2.0", "result":true});
+            return;
+        }
+
+        var sendURL = 'curl -X POST --data' + " '" + '{"jsonrpc":"2.0","method":"eth_sendInvalidWithdrawTransaction","params":['  + '"' + signature  + '",' + '"' + failedTx + '"],"id":1}' + "' " + host + ' -H "content-type: application/json"'
+
+        console.log("eth_sendInvalidWithdrawTransaction url=", sendURL);
+        await child_process.exec(sendURL, function(err, stdout, stderr) {
+            let jsonData = JSON.parse(stdout)
+            if (jsonData["error"] != null) {
+                console.log("stdout", stdout);
+                res.json({"error": null, "id": null, "jsonrpc": "2.0", "result":false});
+                return
+            }
+            console.log("jsonData", jsonData);
+            failedTx=jsonData["result"];
+            res.json({"error": null, "id": null, "jsonrpc": "2.0", "result": false});
+        });
+        return;
+    } catch (err) {
+        console.log("failed deposit transaction by hash error==>", err);
+        common.reterr(err, res);
+        return;
+    }
+}

elastos/Elastos.ELA.SideChain.ESC

internal/ethapi/api.go
@@ -49,6 +49,7 @@ import (
 	"github.com/elastos/Elastos.ELA.SideChain.ETH/rpc"
 	"github.com/elastos/Elastos.ELA.SideChain.ETH/smallcrosstx"
 	"github.com/elastos/Elastos.ELA.SideChain.ETH/spv"
+	"github.com/elastos/Elastos.ELA.SideChain.ETH/withdrawfailedtx"
 
 	"github.com/elastos/Elastos.ELA/events"
 )
@@ -779,6 +780,25 @@ func (s *PublicBlockChainAPI) GetFailedRechargeTxs(ctx context.Context, height u
 func (s *PublicBlockChainAPI) GetFailedRechargeTxByHash(ctx context.Context, hash string) (string, error) {
 	txid := spv.GetFailedRechargeTxByHash(hash)
 	return txid, nil
+}
+
+func (s *PublicBlockChainAPI) SendInvalidWithdrawTransaction(ctx context.Context, signature string, hash string) error {
+	txid := common.HexToHash(hash)
+	tx, _, _, _, err := s.b.GetTransaction(ctx, txid)
+	if tx == nil || err != nil {
+		msg := fmt.Sprintf("not found withdraw tx, txid:%s", txid)
+		log.Error(msg)
+		return errors.New(msg)
+	}
+	if tx.To().String() != s.b.ChainConfig().BlackContractAddr {
+		msg := fmt.Sprintf("is not withdraw tx, txid:%s", hash)
+		return errors.New(msg)
+	}
+	err = withdrawfailedtx.ReceivedFailedWithdrawTx(hash, signature)
+	if err != nil {
+		log.Error("ReceivedFailedWithdrawTx", "error", err)
+	}
+	return err
 }
 
 // CallArgs represents the arguments for a call.

oracle/receivedInvaliedwithrawtx.js
@@ -1,0 +1,44 @@
+"use strict";
+
+const common = require("./common");
+const child_process = require("child_process");
+
+module.exports = async function (json_data, res) {
+    try {
+        let signature = json_data["params"]["signature"];
+        let failedTx = json_data["params"]["txHash"];
+        let tx = failedTx;
+        if (tx.indexOf("0x") !== 0) {
+            tx = "0x" + failedTx;
+        }
+        let host = common.web3._provider.host;
+
+        let txprocessed = await common.web3.eth.getStorageAt(common.blackAdr, tx, common.latest)
+        if (txprocessed != common.zeroHash64) {
+            console.log("Failed Withdraw Trasaction Hash already processed: " + txprocessed);
+            console.log("============================================================");
+            res.json({"error": null, "id": null, "jsonrpc": "2.0", "result":true});
+            return;
+        }
+
+        var sendURL = 'curl -X POST --data' + " '" + '{"jsonrpc":"2.0","method":"eth_sendInvalidWithdrawTransaction","params":['  + '"' + signature  + '",' + '"' + failedTx + '"],"id":1}' + "' " + host + ' -H "content-type: application/json"'
+
+        console.log("eth_sendInvalidWithdrawTransaction url=", sendURL);
+        await child_process.exec(sendURL, function(err, stdout, stderr) {
+            let jsonData = JSON.parse(stdout)
+            if (jsonData["error"] != null) {
+                console.log("stdout", stdout);
+                res.json({"error": null, "id": null, "jsonrpc": "2.0", "result":false});
+                return
+            }
+            console.log("jsonData", jsonData);
+            failedTx=jsonData["result"];
+            res.json({"error": null, "id": null, "jsonrpc": "2.0", "result": false});
+        });
+        return;
+    } catch (err) {
+        console.log("failed withdraw transaction by hash error==>", err);
+        common.reterr(err, res);
+        return;
+    }
+}