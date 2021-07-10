2cdee4ebd59069a8ee20a6b52bc0561471d0c870
ttblack <zhangxiaobin@elastos.org>
2021-07-10 15:02:11 +0800

    init layer2

---
+++ chainbridge-core/relayer/relayer.go
@@ -0,0 +1,67 @@
+// Copyright 2021 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
+package relayer
+
+import (
+	"fmt"
+	"github.com/elastos/Elastos.ELA.SideChain.ETH/log"
+)
+
+type RelayedChain interface {
+	PollEvents(stop <-chan struct{}, sysErr chan<- error, eventsChan chan *Message)
+	Write(message *Message) error
+	ChainID() uint8
+}
+
+func NewRelayer(chains []RelayedChain) *Relayer {
+	return &Relayer{relayedChains: chains}
+}
+
+type Relayer struct {
+	relayedChains []RelayedChain
+	registry      map[uint8]RelayedChain
+}
+
+// Starts the relayer. Relayer routine is starting all the chains
+// and passing them with a channel that accepts unified cross chain message format
+func (r *Relayer) Start(stop <-chan struct{}, sysErr chan error) {
+	log.Debug("Starting relayer")
+	messagesChannel := make(chan *Message)
+	for _, c := range r.relayedChains {
+		log.Debug("Starting chain", "chainid", c.ChainID())
+		r.addRelayedChain(c)
+		go c.PollEvents(stop, sysErr, messagesChannel)
+	}
+	for {
+		select {
+		case m := <-messagesChannel:
+			go r.route(m)
+			continue
+		case _ = <-stop:
+			return
+		}
+	}
+}
+
+// Route function winds destination writer by mapping DestinationID from message to registered writer.
+func (r *Relayer) route(m *Message) {
+	w, ok := r.registry[m.Destination]
+	if !ok {
+		log.Error(fmt.Sprintf("no resolver for destID %v to send message registered", m.Destination))
+		return
+	}
+	log.Debug(fmt.Sprintf("Sending message %+v to destination %v", m, m.Destination))
+	if err := w.Write(m); err != nil {
+		log.Error("rout error", "error", err, "msg", fmt.Sprint(m))
+		return
+	}
+}
+
+func (r *Relayer) addRelayedChain(c RelayedChain) {
+	if r.registry == nil {
+		r.registry = make(map[uint8]RelayedChain)
+	}
+	chainID := c.ChainID()
+	r.registry[chainID] = c
+}
