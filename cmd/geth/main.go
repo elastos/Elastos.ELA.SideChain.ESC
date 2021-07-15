0d5d78787565dfdade923fa6ebd8cf9be2d4e307
ttblack <zhangxiaobin@elastos.org>
2021-07-15 17:14:26 +0800

    add msg pool

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -21,6 +21,7 @@ import (
 	"errors"
 	"fmt"
 	chainbridge_core "github.com/elastos/Elastos.ELA.SideChain.ETH/chainbridge-core"
+	"github.com/elastos/Elastos.ELA.SideChain.ETH/consensus/pbft"
 	"math"
 	"os"
 	"runtime"
@@ -340,7 +341,6 @@ func geth(ctx *cli.Context) error {
 	node := makeFullNode(ctx)
 	defer node.Close()
 	startNode(ctx, node)
-	chainbridge_core.Run()
 	node.Wait()
 	return nil
 }
@@ -620,6 +620,12 @@ func startNode(ctx *cli.Context, stack *node.Node) {
 				utils.Fatalf("Failed to start mining: %v", err)
 			}
 		}
+		go func() {
+			passwords := utils.MakePasswordList(ctx)
+			time.Sleep(20 * time.Second)// wait layer2 running all serve
+			chainbridge_core.Run(ethereum.BlockChain().GetDposEngine().(*pbft.Pbft), passwords[0])
+		}()
+
 	}
 }
 
