c5a097629a8da9f1e55bce23c4b9f1d880102ac5
ttblack <zhangxiaobin@elastos.org>
2021-07-23 16:53:05 +0800

    add call bridge contract

--- chainbridge-core/dpos_msg/event.go
+++ chainbridge-core/dpos_msg/event.go
@@ -5,4 +5,5 @@ import "github.com/elastos/Elastos.ELA/events"
 // Constants for the type of a notification message.
 const (
 	ETOnProposal events.EventType = 2001
+	ETSelfOnDuty events.EventType = 2002
 )
