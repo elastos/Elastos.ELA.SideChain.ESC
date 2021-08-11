032a460bc96bb49e1d70f5d856af49192cce8057
ttblack <zhangxiaobin@elastos.org>
2021-08-11 16:55:31 +0800

    modify batch logic

--- chainbridge-core/engine/engine.go
+++ chainbridge-core/engine/engine.go
@@ -1,12 +1,14 @@
 package engine
 
 import (
+	"github.com/elastos/Elastos.ELA/dpos/p2p/peer"
 	"github.com/elastos/Elastos.ELA/p2p"
 )
 
 type ESCEngine interface {
 	IsOnduty() bool
 	SendMsgProposal(proposalMsg p2p.Message)
+	SendMsgToPeer(proposalMsg p2p.Message, pid peer.PID)
 	SignData(data []byte) []byte
 	GetProducer() []byte
 	GetTotalProducerCount() int
