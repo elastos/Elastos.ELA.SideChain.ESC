9723277644e56c4406f5441c2bb86faee9c1172e
ttblack <zhangxiaobin@elastos.org>
2021-08-15 15:45:27 +0800

    collect eth public key

---
+++ chainbridge-core/dpos_msg/darbiter.go
@@ -0,0 +1,88 @@
+package dpos_msg
+
+import (
+	"bytes"
+	"io"
+	"time"
+
+	"github.com/elastos/Elastos.ELA/common"
+	"github.com/elastos/Elastos.ELA/crypto"
+	"github.com/elastos/Elastos.ELA/p2p"
+)
+
+const (
+	// maxCipherLength indicates the max length of the address cipher.
+	maxCipherLength = 256
+)
+
+// Ensure BatchMsg implement p2p.Message interface.
+var _ p2p.Message = (*DArbiter)(nil)
+
+type DArbiter struct {
+	// The peer ID indicates who's address it is.
+	PID [33]byte
+
+	// Timestamp represents the time when this message created.
+	Timestamp time.Time
+
+	// Which peer ID is used to encode the address cipher.
+	Encode [33]byte
+
+	// The encrypted network address using the encode peer ID.
+	Cipher []byte
+
+	// Signature of the encode peer ID and cipher to proof the sender itself.
+	Signature []byte
+}
+
+func (m *DArbiter) CMD() string {
+	return CmdDArbiter
+}
+
+func (m *DArbiter) MaxLength() uint32 {
+	return 387 // 33+33+256+65
+}
+
+func (m *DArbiter) Serialize(w io.Writer) error {
+	var timestamp = m.Timestamp.Unix()
+	err := common.WriteElements(w, m.PID, timestamp, m.Encode)
+	if err != nil {
+		return err
+	}
+
+	if err := common.WriteVarBytes(w, m.Cipher); err != nil {
+		return err
+	}
+
+	return common.WriteVarBytes(w, m.Signature)
+}
+
+func (m *DArbiter) Deserialize(r io.Reader) error {
+	var timestamp int64
+	err := common.ReadElements(r, &m.PID, &timestamp, &m.Encode)
+	if err != nil {
+		return err
+	}
+	m.Timestamp = time.Unix(timestamp, 0)
+
+	m.Cipher, err = common.ReadVarBytes(r, maxCipherLength, "DArbiter.Cipher")
+	if err != nil {
+		return err
+	}
+
+	m.Signature, err = common.ReadVarBytes(r, crypto.SignatureLength,
+		"DArbiter.Signature")
+	return err
+}
+
+func (m *DArbiter) Data() []byte {
+	b := new(bytes.Buffer)
+	var timestamp = m.Timestamp.Unix()
+	common.WriteElements(b, timestamp, m.Encode)
+	common.WriteVarBytes(b, m.Cipher)
+	return b.Bytes()
+}
+
+func (m *DArbiter) Hash() common.Uint256 {
+	return common.Sha256D(m.Data())
+}
--- chainbridge-core/engine/engine.go
+++ chainbridge-core/engine/engine.go
@@ -1,6 +1,8 @@
 package engine
 
 import (
+	"github.com/elastos/Elastos.ELA.SideChain.ESC/chainbridge-core/crypto"
+
 	"github.com/elastos/Elastos.ELA/dpos/p2p/peer"
 	"github.com/elastos/Elastos.ELA/p2p"
 )
@@ -10,8 +12,9 @@ type ESCEngine interface {
 	SendMsgProposal(proposalMsg p2p.Message)
 	SendMsgToPeer(proposalMsg p2p.Message, pid peer.PID)
 	SignData(data []byte) []byte
+	DecryptArbiter(cipher []byte) (arbiter []byte, err error)
 	GetProducer() []byte
-	GetBridgeArbiters() [][]byte
+	GetBridgeArbiters() crypto.Keypair
 	GetTotalProducerCount() int
 	IsSyncFinished() bool
 }
