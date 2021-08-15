ca69d945f909de1174265bd14d1cd15744cbdfe9
ttblack <zhangxiaobin@elastos.org>
2021-08-15 15:45:27 +0800

    collect eth public key

--- dpos/account.go
+++ dpos/account.go
@@ -6,6 +6,8 @@
 package dpos
 
 import (
+	"github.com/elastos/Elastos.ELA.SideChain.ESC/chainbridge-core/crypto"
+	"github.com/elastos/Elastos.ELA.SideChain.ESC/chainbridge-core/crypto/secp256k1"
 	"github.com/elastos/Elastos.ELA/account"
 	daccount "github.com/elastos/Elastos.ELA/dpos/account"
 )
@@ -16,4 +18,12 @@ func GetDposAccount(keystorePath string, password []byte) (daccount.Account, err
 		return nil, err
 	}
 	return daccount.New(client.GetMainAccount()), nil
+}
+
+func GetBridgeAccount(keystorePath string, password []byte) (crypto.Keypair, error) {
+	client, err := account.Open(keystorePath, password)
+	if err != nil {
+		return nil, err
+	}
+	return secp256k1.NewKeypairFromPrivateKey(client.GetMainAccount().PrivateKey)
 }
