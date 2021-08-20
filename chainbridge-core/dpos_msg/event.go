50554fe1d900738b44385c071f737b80dac451a2
ttblack <zhangxiaobin@elastos.org>
2021-08-20 22:13:18 +0800

    modify collect arbiter logic

--- chainbridge-core/dpos_msg/event.go
+++ chainbridge-core/dpos_msg/event.go
@@ -4,7 +4,8 @@ import "github.com/elastos/Elastos.ELA/events"
 
 // Constants for the type of a notification message.
 const (
-	ETOnProposal events.EventType = 2001
-	ETSelfOnDuty events.EventType = 2002
-	ETOnArbiter events.EventType = 2003
+	ETOnProposal     events.EventType = 2001
+	ETSelfOnDuty     events.EventType = 2002
+	ETOnArbiter      events.EventType = 2003
+	ETRequireArbiter events.EventType = 2004
 )
