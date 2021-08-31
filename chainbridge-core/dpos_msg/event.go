c1d5d28e9e56141503b62330d4676b0675c41b6e
ttblack <zhangxiaobin@elastos.org>
2021-08-31 14:42:38 +0800

    collect update arbiters signature

--- chainbridge-core/dpos_msg/event.go
+++ chainbridge-core/dpos_msg/event.go
@@ -4,8 +4,10 @@ import "github.com/elastos/Elastos.ELA/events"
 
 // Constants for the type of a notification message.
 const (
-	ETOnProposal     events.EventType = 2001
-	ETSelfOnDuty     events.EventType = 2002
-	ETOnArbiter      events.EventType = 2003
-	ETRequireArbiter events.EventType = 2004
+	ETOnProposal         events.EventType = 2001
+	ETSelfOnDuty         events.EventType = 2002
+	ETOnArbiter          events.EventType = 2003
+	ETRequireArbiter     events.EventType = 2004
+	ETReqArbiterSig      events.EventType = 2005
+	ETFeedBackArbiterSig events.EventType = 2006
 )
