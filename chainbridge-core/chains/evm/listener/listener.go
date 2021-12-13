3a3487b5e41e4c2ddd4802166ba36f98946c950b
ttblack <zhangxiaobin@elastos.org>
2021-12-13 09:30:16 +0800

    l1->l2 setfee

--- chainbridge-core/chains/evm/listener/listener.go
+++ chainbridge-core/chains/evm/listener/listener.go
@@ -26,11 +26,18 @@ type DepositRecord struct {
 	DepositNonce                   uint64
 	Depositer                      common.Address
 	Amount                         *big.Int
+	Fee 						   *big.Int
+}
+
+type ChangeSuperSigner struct {
+	OldSuperSigner common.Address
+	NewSuperSigner common.Address
 }
 
 type ChainClient interface {
 	LatestBlock() (*big.Int, error)
 	FetchDepositLogs(ctx context.Context, address common.Address, startBlock *big.Int, endBlock *big.Int) ([]*DepositRecord, error)
+	FetchChangeSuperSigner(ctx context.Context, address common.Address, startBlock *big.Int, endBlock *big.Int) ([]*ChangeSuperSigner, error)
 	CallContract(ctx context.Context, callArgs map[string]interface{}, blockNumber *big.Int) ([]byte, error)
 }
 
@@ -48,9 +55,12 @@ func NewEVMListener(chainReader ChainClient, handler EventHandler, bridgeAddress
 	return &EVMListener{chainReader: chainReader, eventHandler: handler, bridgeAddress: bridgeAddress}
 }
 
-func (l *EVMListener) ListenToEvents(startBlock *big.Int, chainID uint8, kvrw blockstore.KeyValueWriter, stopChn <-chan struct{}, errChn chan<- error) <-chan *relayer.Message {
-	// TODO: This channel should be closed somewhere!
+func (l *EVMListener) ListenToEvents(startBlock *big.Int, chainID uint8,
+	                                 kvrw blockstore.KeyValueWriter,
+	                                 stopChn <-chan struct{},
+	                                 errChn chan<- error) (<-chan *relayer.Message, <-chan *relayer.ChangeSuperSigner) {
 	ch := make(chan *relayer.Message)
+	changeSuperCh := make(chan *relayer.ChangeSuperSigner)
 	go func() {
 		for {
 			select {
@@ -67,41 +77,21 @@ func (l *EVMListener) ListenToEvents(startBlock *big.Int, chainID uint8, kvrw bl
 					time.Sleep(BlockRetryInterval)
 					continue
 				}
-				log.Info("FetchDepositLogs", "startBlock", startBlock.Uint64(), "chainID", chainID)
-				logs, err := l.chainReader.FetchDepositLogs(context.Background(), l.bridgeAddress, startBlock, startBlock)
+				err = l.fetchDepositMsg(startBlock, chainID, ch)
 				if err != nil {
-					// Filtering logs error really can appear only on wrong configuration or temporary network problem
-					// so i do no see any reason to break execution
-					log.Error("FetchDepositLogs errors", "ChainID", chainID)
-					continue
+					errChn <- err
+					return
 				}
-				for _, eventLog := range logs {
-					err := l.eventHandler.HandleEvent(chainID, eventLog.DestinationChainID, eventLog.DepositNonce, eventLog.ResourceID)
-					if err != nil {
-						errChn <- err
-						log.Error("HandleEvent error", "error", err)
-						return
-					}
 
-					m := &relayer.Message{
-						Source: chainID,
-						Destination: eventLog.DestinationChainID,
-						DepositNonce: eventLog.DepositNonce,
-						ResourceId: eventLog.ResourceID,
-						Payload: []interface{}{
-							eventLog.Amount.Bytes(),
-							eventLog.Depositer,
-						},
-						Type: relayer.FungibleTransfer,
-					}
-					log.Info(fmt.Sprintf("Resolved message %+v in block %s", m, startBlock.String()))
-					ch <- m
+				err = l.fetchChangeSuperSigner(startBlock,chainID, changeSuperCh)
+				if err != nil {
+					errChn <- err
+					return
 				}
 				if startBlock.Int64()%20 == 0 {
 					// Logging process every 20 bocks to exclude spam
 					log.Debug("Queried block for deposit events", "block", startBlock.String(), "chainId", chainID)
 				}
-				// TODO: We can store blocks to DB inside listener or make listener send something to channel each block to save it.
 				//Write to block store. Not a critical operation, no need to retry
 				err = blockstore.StoreBlock(kvrw, startBlock, chainID)
 				if err != nil {
@@ -112,5 +102,59 @@ func (l *EVMListener) ListenToEvents(startBlock *big.Int, chainID uint8, kvrw bl
 			}
 		}
 	}()
-	return ch
+	return ch, changeSuperCh
+}
+
+func (l *EVMListener) fetchDepositMsg(startBlock *big.Int, chainID uint8, msg chan *relayer.Message) error {
+	log.Info("FetchDepositLogs", "startBlock", startBlock.Uint64(), "chainID", chainID)
+	logs, err := l.chainReader.FetchDepositLogs(context.Background(), l.bridgeAddress, startBlock, startBlock)
+	if err != nil {
+		// Filtering logs error really can appear only on wrong configuration or temporary network problem
+		// so i do no see any reason to break execution
+		log.Error("FetchDepositLogs errors", "ChainID", chainID)
+		return nil
+	}
+	for _, eventLog := range logs {
+		err = l.eventHandler.HandleEvent(chainID, eventLog.DestinationChainID, eventLog.DepositNonce, eventLog.ResourceID)
+		if err != nil {
+			log.Error("HandleEvent error", "error", err)
+			return err
+		}
+
+		m := &relayer.Message{
+			Source: chainID,
+			Destination: eventLog.DestinationChainID,
+			DepositNonce: eventLog.DepositNonce,
+			ResourceId: eventLog.ResourceID,
+			Payload: []interface{}{
+				eventLog.Amount.Bytes(),
+				eventLog.Depositer,
+				eventLog.Fee.Bytes(),
+			},
+		}
+		msg <- m
+		log.Info(fmt.Sprintf("Resolved message %+v in block %s", m, startBlock.String()))
+	}
+	return nil
+}
+
+
+func (l *EVMListener) fetchChangeSuperSigner(startBlock *big.Int, chainID uint8, msg chan *relayer.ChangeSuperSigner) error {
+	logs, err := l.chainReader.FetchChangeSuperSigner(context.Background(), l.bridgeAddress, startBlock, startBlock)
+	if err != nil {
+		// Filtering logs error really can appear only on wrong configuration or temporary network problem
+		// so i do no see any reason to break execution
+		log.Error("FetchChangeSuperSigner errors", "ChainID", chainID)
+		return nil
+	}
+	for _, eventLog := range logs {
+		m := &relayer.ChangeSuperSigner{
+			SourceChain: chainID,
+			OldSuperSigner: eventLog.OldSuperSigner,
+			NewSuperSigner: eventLog.NewSuperSigner,
+		}
+		msg <- m
+		log.Info(fmt.Sprintf("Resolved change super msg %+v in block %s", m.NewSuperSigner.String(), startBlock.String()))
+	}
+	return nil
 }
