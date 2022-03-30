0c008bf2437f18b9c06c030ead8d6833717a5aff
zhangbin <zhangbin@zhangbindeMacBook-Pro.local>
2022-03-30 19:45:28 +0800

    modify abi change abiter to arbiter

--- chainbridge_abi/utils.go
+++ chainbridge_abi/utils.go
@@ -7,31 +7,31 @@ import (
 )
 
 func GetSetArbitersABI() (abi.ABI, error) {
-	definition := "[{\"inputs\": [{\"internalType\": \"address[]\",\"name\": \"_addressList\",\"type\": \"address[]\"},{\"internalType\": \"uint256\",\"name\": \"_addressCount\",\"type\": \"uint256\"},{\"internalType\": \"bytes[]\",\"name\": \"_sigList\",\"type\": \"bytes[]\"}],\"name\": \"setAbiterList\",\"outputs\": [],\"stateMutability\": \"nonpayable\",\"type\": \"function\"}]"
+	definition := "[{\"inputs\": [{\"internalType\": \"address[]\",\"name\": \"_addressList\",\"type\": \"address[]\"},{\"internalType\": \"uint256\",\"name\": \"_addressCount\",\"type\": \"uint256\"},{\"internalType\": \"bytes[]\",\"name\": \"_sigList\",\"type\": \"bytes[]\"}],\"name\": \"setArbiterList\",\"outputs\": [],\"stateMutability\": \"nonpayable\",\"type\": \"function\"}]"
 	a, err := abi.JSON(strings.NewReader(definition))
 	return a, err
 }
 
 func GetArbitersABI() (abi.ABI, error) {
-	definition := "[{\"inputs\":[],\"name\":\"getAbiterList\",\"outputs\":[{\"internalType\":\"address[]\",\"name\":\"\",\"type\":\"address[]\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
+	definition := "[{\"inputs\":[],\"name\":\"getArbiterList\",\"outputs\":[{\"internalType\":\"address[]\",\"name\":\"\",\"type\":\"address[]\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
 	a, err := abi.JSON(strings.NewReader(definition))
 	return a, err
 }
 
 func GetSignaturesABI() (abi.ABI, error) {
-	definition := "[{\"inputs\": [],\"name\": \"getAbiterSigs\",\"outputs\": [{\"internalType\": \"bytes[]\",\"name\": \"\",\"type\": \"bytes[]\"}],\"stateMutability\": \"view\",\"type\": \"function\"}]"
+	definition := "[{\"inputs\": [],\"name\": \"getArbiterSigs\",\"outputs\": [{\"internalType\": \"bytes[]\",\"name\": \"\",\"type\": \"bytes[]\"}],\"stateMutability\": \"view\",\"type\": \"function\"}]"
 	a, err := abi.JSON(strings.NewReader(definition))
 	return a, err
 }
 
 func GetTotalCountABI() (abi.ABI, error) {
-	definition := "[{\"inputs\": [],\"name\": \"getAbiterCount\",\"outputs\": [{\"internalType\": \"uint256\",\"name\": \"\",\"type\": \"uint256\"}],\"stateMutability\": \"view\",\"type\": \"function\"}]"
+	definition := "[{\"inputs\": [],\"name\": \"getArbiterCount\",\"outputs\": [{\"internalType\": \"uint256\",\"name\": \"\",\"type\": \"uint256\"}],\"stateMutability\": \"view\",\"type\": \"function\"}]"
 	a, err := abi.JSON(strings.NewReader(definition))
 	return a, err
 }
 
 func UpdateArbiterABI() (abi.ABI, error) {
-	definition := "[{\"anonymous\": false,\"inputs\": [{\"indexed\": false,\"internalType\": \"uint256\",\"name\": \"_addressCount\",\"type\": \"uint256\"}],\"name\": \"SetAbiterList\",\"type\": \"event\"}]"
+	definition := "[{\"anonymous\": false,\"inputs\": [{\"indexed\": false,\"internalType\": \"uint256\",\"name\": \"_addressCount\",\"type\": \"uint256\"}],\"name\": \"SetArbiterList\",\"type\": \"event\"}]"
 	a, err := abi.JSON(strings.NewReader(definition))
 	return a, err
 }
