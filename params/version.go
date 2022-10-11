fda2a84a52fc47f5388ddc5b961671386b95bbf8
zhangbin <zhangbin@zhangbindeMacBook-Pro.local>
2022-10-11 22:27:43 +0800

    modify version and checkpoint

--- params/version.go
+++ params/version.go
@@ -25,7 +25,7 @@ const (
 	VersionMinor = 9        // Minor version component of the current release
 	VersionPatch = 7        // Patch version component of the current release
 	VersionCross = 0        // Cross version component of the current release
-	VersionMeta  = "v0.2.1" // Version metadata to append to the version string
+	VersionMeta  = "v0.2.2" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
@@ -44,7 +44,8 @@ var VersionWithMeta = func() string {
 
 // ArchiveVersion holds the textual version string used for Geth archives.
 // e.g. "1.8.11-dea1ce05" for stable releases, or
-//      "1.8.13-unstable-21c059b6" for unstable releases
+//
+//	"1.8.13-unstable-21c059b6" for unstable releases
 func ArchiveVersion(gitCommit string) string {
 	vsn := Version
 	if VersionMeta != "stable" {
