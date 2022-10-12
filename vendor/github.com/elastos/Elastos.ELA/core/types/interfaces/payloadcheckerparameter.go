3d674a1cd148a9377879c4e54ac164081effe97f
zhangbin <zhangbin@zhangbindeMacBook-Pro.local>
2022-10-12 20:27:08 +0800

    modify go mod by go 1.16

--- vendor/github.com/elastos/Elastos.ELA/core/types/interfaces/payloadcheckerparameter.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2017-2021 The Elastos Foundation
-// Use of this source code is governed by an MIT
-// license that can be found in the LICENSE file.
-//
-
-package interfaces
-
-type Parameters interface {
-}
