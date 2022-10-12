8138bb7e23ae3330c305cba06fad4377528ac92a
zhangbin <zhangbin@zhangbindeMacBook-Pro.local>
2022-10-12 20:27:08 +0800

    modify go mod by go 1.16

--- vendor/github.com/elastos/Elastos.ELA.SideChain/vm/interfaces/crypto.go
+++
@@ -1,9 +0,0 @@
-package interfaces
-
-type ICrypto interface {
-	Hash168(data []byte) []byte
-
-	Hash256(data []byte) []byte
-
-	VerifySignature(data []byte, signature []byte, pubkey []byte) error
-}
